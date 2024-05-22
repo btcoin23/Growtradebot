cfa50e5f33e0ae49d5811dbdd029fc46c7f61d21
hu050 <david.hu.info@gmail.com>
2024-05-23 01:55:17 +0800

    fixed issue

--- src/config.ts
+++ src/config.ts
@@ -40,5 +40,5 @@ export const GrowTradeVersion = '| beta';
 
 export const GROWSOL_API_ENDPOINT = process.env.GROWSOL_API_ENDPOINT || "http://127.0.0.1:5001";
 
-export const PNL_SHOW_THRESHOLD_USD = 1;
-export const RAYDIUM_PASS_TIME = 1200 * 60 * 1000; // 1200 minutes * 60 seconds * 1000 millisecons
+export const PNL_SHOW_THRESHOLD_USD = 5;
+export const RAYDIUM_PASS_TIME = 180 * 60 * 1000; // 1200 minutes * 60 seconds * 1000 millisecons
